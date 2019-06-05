ec31b9da8c1edcf1109e1addb9f73cbf2bbcdf03
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-06-05 13:58:31 -0700

    Initial Commit

---
+++ FFMPEGShortcutsApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FFMPEGShortcutsApp
+//
+//  Created by Zhuowei Zhang on 2019-06-05.
+//  Copyright © 2019 Zhuowei Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
